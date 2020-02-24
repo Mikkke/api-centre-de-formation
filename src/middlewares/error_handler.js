be99690856b8d465eb4ff8469262d09dfc81422c
Sandro Micalizzi <54776082+sandrocode@users.noreply.github.com>
2020-02-24 03:11:18 +0100

    Création des routeurs et contrôleurs pour les formations et les promotions, et configuration de quelques routes

--- src/middlewares/error_handler.js
+++ src/middlewares/error_handler.js
@@ -1,11 +1,22 @@
+const { SERVER_ERROR } = require("../helpers/status_code");
+
 // eslint-disable-next-line no-unused-vars
 const errorHandler = (error, request, response, next) => {
   const { status, title, detail } = error;
 
+  if (!status) {
+    response.status(SERVER_ERROR);
+    response.json({
+      titre: "Oups ! Quelque chose ne fonctionne pas !",
+      description:
+        "Le serveur rencontre un problème inconnu. Veuillez réessayer plus tard."
+    });
+  }
+
   response.status(status);
   response.json({
     titre: title,
-    detail
+    description: detail
   });
 };
 
